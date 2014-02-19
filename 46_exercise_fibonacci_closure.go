d85e43a5f8670c6fb64fed97c7b296e80d24fa3c
Kazuyuki Honda <hakobera@gmail.com>
2014-02-19 13:29:15 +0900

    Fix 46

--- 46_exercise_fibonacci_closure.go
+++ 46_exercise_fibonacci_closure.go
@@ -5,23 +5,10 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that retutns an int.
 func fibonacci() func() int {
-  var f func(i int) int
-
-  f = func(i int) int {
-    if (i == 0) {
-      return 0
-    } else if (i == 1) {
-      return 1
-    } else {
-      return f(i-1) + f(i-2)
-    }
-  }
-
-  n := 0
+  n1, n2 := 0, 1
   return func() int {
-    fib := f(n)
-    n++
-    return fib
+    n1, n2 = n1 + n2, n1
+    return n1
   }
 }
 
